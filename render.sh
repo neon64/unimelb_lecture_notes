e2c9b251af311a8dcecccf98de24354f331b84dd
Chris Chamberlain <chris@chamberlain.id.au>
2020-10-30 23:55:22 +1100

    Changed dirs

--- render.sh
+++ render.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
+# since GitHub README doesn't render LaTeX math expressions properly, we instead
+# resort to converting all LaTeX to svg.
+# using: https://github.com/leegao/readme2tex
+
 git branch -D svgs || exit
 git branch svgs || exit
 git checkout svgs || exit
 
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
 for DIR in comp10002/pattern_search/ comp10002/
 do
+    cd "$SCRIPTPATH" || exit
     cd "$DIR" || exit
     for FILE in *.md
     do
